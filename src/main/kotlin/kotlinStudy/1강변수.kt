9c9ebe56c8b089770fcc09c34668aafa707d8a98
JW <rlawjdtm123@naver.com>
2022-01-02 14:15:51 +0900

    변수 복습

--- src/main/kotlin/kotlinStudy/1강변수.kt
+++ src/main/kotlin/kotlinStudy/1강변수.kt
@@ -2,9 +2,24 @@ package kotlinStudy
 
 fun main() {
     println("Kotlin is so fun")
-    println("Kotlin is so fun")
-    println("Kotlin is so fun")
-    println("Kotlin is so fun")
-    println("Kotlin is so fun")
+
+    val a: Int?= null
+    var b: Double = 123.56
+    var c: Float = 123.55f
+    var d = 111.23f
+    var e: Boolean = true
+    var f: Boolean = false
+    var g: Char = 'a'
+    var h = "sdasd"
+    var i = """asdasdasdas
+        asdasdasd
+        asdasdasdsadas alcas!@!@???!@! $a
+    """
+
+
+    print("$a\n")
+    println(i)
+
+
 
 }
